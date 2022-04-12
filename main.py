f7ce94ad92a80720de107234520fa850f6266c3b
Arina Yefimova <e-mail>
2022-04-12 17:16:44 +0300

    lolo

--- main.py
+++ main.py
@@ -12,15 +12,3 @@ def main():
     screen = pygame.display.set_mode(DISPLAY)
     bg = Surface((WIN_WIDTH, WIN_HEIGHT))
     bg.fill(Color(BACKGROUND_COLOR))
-
-    while 1:
-        for e in pygame.event.get():
-            if e.type == QUIT:
-                raise SystemExit, "QUIT"
-        screen.blit(bg, (0, 0))
-        pygame.display.update()
-
-
-if __name__ == "__main__":
-    main()
-# lala
