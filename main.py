a35ef3ee0ad5a6e84e2a22e26bcbb5922bdc82b8
ElementSi <eg.zzz.747@gmail.com>
2022-04-18 18:05:55 +0300

    Начал расписывать шаблоны классов и main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import random as rnd
+import map_objects as obj
 
 
 pg.init()
@@ -8,3 +9,96 @@ FPS = 60
 SCREEN_WIDTH = pg.display.Info().current_w
 SCREEN_HEIGHT = pg.display.Info().current_h
 
+
+class Gameplay:
+    """
+    Gameplay itself
+    """
+    def __init__(self, surface):
+        """
+        Initialising of Gameplay
+        :param surface: Pygame Surface object - target surface
+        """
+        self.surface = surface
+        self.clock = pg.time.Clock()
+        self.map = None  # Need to add map class
+        self.list_solid_object = []  # Need to add settler, animals, nature objects, maybe constructions of loot
+        self.list_effects = []
+        self.list_loot = []
+        self.chosen_map_object = None
+        self.chosen_task = None
+        self.finished = False
+
+    def create_new_animal(self):
+        """
+        Randomly spawn new animal on the border of the map
+        """
+
+    def draw_map(self):
+        """
+        Drawing every map tile
+        """
+
+    def draw_objects(self):
+        """
+        Drawing every object
+        """
+
+    def display_update(self):
+        """
+        Updating display to reflect changes of objects
+        """
+        pg.display.update()
+        self.clock.tick(FPS)
+
+    def process_input(self):
+        """
+        Processing all player input
+        """
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.finished = True
+
+            elif event.type == pg.KEYDOWN:
+                if event.key == 27:  # Esk.key = 27
+                    if self.chosen_map_object is None:
+                        self.finished = True
+
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if self.chosen_task is None:
+                    for map_object in self.list_solid_object:
+                        if map_object.chose(event):
+                            self.chosen_map_object = map_object
+                            break
+
+    def move_creation(self):
+        """
+        Moving every creation
+        """
+
+    def ai_acts(self):
+        """
+        Acting of artificial intelligence of animals
+        """
+
+    def remove_solid_object(self):
+        """
+        Removing dead animals and settler from lists
+        """
+        pass
+
+    def process_effects(self):
+        """
+        Increase of particle age and removing particles that are too old
+        """
+        pass
+
+
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+game = Gameplay(screen)
+
+while not game.finished:
+    game.process_input()
+
+pg.quit()
--- map_objects.py
+++ map_objects.py
@@ -6,107 +6,136 @@ from map import TILE_SIZE
 
 class MapObject:
     """
-    Все объекты, размещённые на карте
+    Any changeable map object
     """
     def __init__(self, surface, coord):
         """
-        Универсальный базовый конструктор объектов
-        :param surface: Pygame Surface object - Окно, в котором отображается объект.
-        :param coord: list[float, float] - Координаты верхнего левого угла объекта.
+        Universal basic constructor of map object
+        :param surface: Pygame Surface object - target window
+        :param coord: list[float, float] - coordinates of upper left corner of object (tile coordinates)
         """
         self.surface = surface
-        self.is_alive = 1
-        self.is_chosen = 0
         self.coord = coord
         self.draw_box = (coord[0] * TILE_SIZE, coord[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE)
         self.texture = pg.transform.scale(pg.image.load("textures/default.png"), (TILE_SIZE, TILE_SIZE))
+        self.is_chosen = False
         self.type = "def_object"
+
+    def chose(self, event):
+        """
+        Choice object be mouse click
+        :param event: Pygame event object - any event from queue
+        :return: bool - is object chosen
+        """
+        pass  # Надо провести аккуратную и быструю (!) проверку на то, попал ли игрок курсором на объект
 
     def draw(self):
         """
-        Отрисовка любого объекта на карте
+        Drawing object in the current window
         """
         self.surface.blit(self.texture, self.draw_box)
+        if self.is_chosen:
+            pass  # Надо нарисовать белую рамку вокруг объекта, если он выбран
+
+    def safe(self, file):
+        """
+        Writing information about an object to a save file
+        :param file: TextIO object - Output stream to an external save file
+        """
+        pass  # Надо записать строчку с исчерпывающей и унифицированной информацией об объекте в файл
 
 
-class Creation(MapObject):
+class SolidObject(MapObject):
     """
-    Живые существа, размещённые на карте
+    Material object with limited hit points that can only exist on the map
     """
     def __init__(self, surface, coord):
         """
-        Универсальный конструктор живых существ
-        :param surface: Pygame Surface object - Окно, в котором отображается объект.
-        :param coord: list[float, float] - Координаты верхнего левого угла объекта.
+        Universal constructor of solid object
+        :param surface: Pygame Surface object - target window
+        :param coord: list[float, float] - coordinates of upper left corner of object (tile coordinates)
         """
         super().__init__(surface, coord)
         self.hit_points = 10
+        self.type = "def_solid_object"
+
+
+class Creation(SolidObject):
+    """
+    Creature that can move and proceed more complex tasks
+    """
+    def __init__(self, surface, coord):
+        """
+        Universal constructor of creature
+        :param surface: Pygame Surface object - target window
+        :param coord: list[float, float] - coordinates of upper left corner of object (tile coordinates)
+        """
+        super().__init__(surface, coord)
+        self.speed = 0.05  # Base value [tile/tick]
+        self.damage = 1  # Base value [hit point]
+        self.melee_cooldown = 60  # Base value [tick]
         self.type = "def_creation"
 
-    def taking_damage(self):
+    def take_damage(self, damage):
         """
-        Получение урона живым существом
+        Taking damage by creature
         """
-        # rood = Object(self.surface, "blood.png", [10, 10])  # найти и спользовать фотографию крови
+        # rood = Object(self.surface, "blood.png", [10, 10])  # Найти и использовать фотографию крови
         # rood.draw()
         # # pygame.display.update()
         # # clock.tick(1)
         # self.live -= 1
 
+    def pathfinder(self, goal_coord, region_map):
+        """
+
+        :param goal_coord:
+        :param region_map:
+        :return:
+        """
+
 
 class Animal(Creation):
     """
-    Животные (управляемые ИИ), размещённые на карте
+    Animals controlled by AI
     """
     def __init__(self, surface, coord):
         """
-        Универсальный конструктор животных
-        :param surface: Pygame Surface object - Окно, в котором отображается объект.
-        :param coord: list[float, float] - Координаты верхнего левого угла животного.
+        Universal constructor of animal
+        :param surface: Pygame Surface object - target window
+        :param coord: list[float, float] - coordinates of upper left corner of object (tile coordinates)
         """
-        super().__init__(self, surface, coord)
-        self.type = "animal"
+        super().__init__(surface, coord)
+        self.activity_rate = 0.1  # Base value, some relative coefficient
+        self.type = "def_animal"
 
     def death(self):
         """
         Processing of death effects
         """
-        self.texture = "died_animal.png"  # найти и спользовать фотографию мертвого животного
-
-    def move(self, obj):
-        """
-        перемещение животного
-        """
-        self.vx = rnd.choice([-10, 0, 10])
-        self.vy = rnd.choice([-10, 0, 10])
-        self.x += self.vx
-        self.y += self.vy
-        if self.hit_test(obj):
-            self.vx *= -1
         pass
 
-    def doing_something(self):
+    def move(self):
         """
-        выполнение какаих-то действий
+        Moving to its goal
         """
         pass
 
 
-class Man(Creation):
+class Settler(Creation):
     """
-    конструктор человека
+    Settler controlled by player
     """
 
-    def __init__(self, surface, texture, size, x, y):
+    def __init__(self, surface, coord):
         """
-        :param surface: экран, где рисуем
-        :param texture: изображение человека
-        :param size: размеры существа
-        :param x: местоположение сущесвта по ОХ
-        :param y: местоположение существа по ОУ
+        Universal constructor of animal
+        :param surface: Pygame Surface object - target window
+        :param coord: list[float, float] - coordinates of upper left corner of object (tile coordinates)
         """
-        super().__init__(surface, texture, size, x, y)
-        self.type = "man"
+        super().__init__(surface, coord)
+        self.activity_rate = 0.1  # Base value, some relative coefficient
+        self.type = "def_animal"
 
     def death(self):
         """
